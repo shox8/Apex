cef1c8f74cde95755783082843821d4f5eb01ce1
Shoxruh <138918930+Shox2008@users.noreply.github.com>
2024-07-18 16:32:09 +0500

    Update

--- app/api/auth/register/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: NextRequest) {
-  const data = await request.json();
-
-  return NextResponse.json(data);
-}
